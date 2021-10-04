593cd1b5ac0533c1f887c90b079a55a441b9e8ee
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-10-04 22:07:57 +0600

    showing table view in admin site

--- store/models/product.py
+++ store/models/product.py
@@ -1,7 +1,11 @@
 from django.db import models
+# To link product with the category as foreign key
+from .category import Category
 
 class Product(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField(default=0)
+    # To link product with the category as foreign key
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
     description = models.CharField(max_length=200, default='')
     image = models.ImageField(upload_to='uploads/products/')
