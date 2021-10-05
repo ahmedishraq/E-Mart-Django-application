ae0156661eeee992cc3679f5d8cb381d6b9fd82f
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-10-05 20:13:55 +0600

    listing categories in Home page

--- store/views.py
+++ store/views.py
@@ -1,10 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models.product import Product
+from .models.category import Category
 
 # Create your views here.
 def index(request):
 
-    prds = Product.get_all_products();
+    products = Product.get_all_products();
+    categories = Category.get_all_categories()
+
+    data={}
+    data['products'] = products
+    data['categories'] = categories
+
     #return render (request, 'orders/order.html')
-    return render (request, 'index.html', {'products' : prds})
+    return render (request, 'index.html', data)
