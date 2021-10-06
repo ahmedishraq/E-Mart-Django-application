addf23ce58e43e58b6e257a7ca9af86bacac5712
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-10-07 01:42:04 +0600

    create views in separate file as python package

---
+++ store/views/home.py
@@ -0,0 +1,34 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from store.models.product import Product
+from store.models.category import Category
+
+
+
+# Create your views here.
+
+# def for product
+def index(request):
+    products = None
+    categories = Category.get_all_categories()
+
+    categoryID = request.GET.get('category')
+    if categoryID:
+        products = Product.get_all_products_by_category_id(categoryID)
+    else:
+        products = Product.get_all_products();
+
+    data = {}
+    data['products'] = products
+    data['categories'] = categories
+
+    # return render (request, 'orders/order.html')
+    return render(request, 'index.html', data)
+
+
+
+
+
+
+
+
---
+++ store/views/login.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth.hashers import check_password
+from store.models.customer import Customer
+from django.views import View
+
+
+# class for login (wiil handle GET and POST method)
+class Login(View):
+    def get(self, request):
+        return render(request, 'login.html')
+
+    def post(self, request):
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        customer = Customer.get_customer_by_email(email)
+        error_message = None
+        if customer:
+            flag = check_password(password, customer.password)
+            if flag:
+                return redirect('homepage')
+            else:
+                error_message = "Email or Password invalid !!"
+                return render(request, 'login.html', {'error': error_message})
+        else:
+            error_message = "Email or Password invalid !!"
+            return render(request, 'login.html', {'error': error_message})
