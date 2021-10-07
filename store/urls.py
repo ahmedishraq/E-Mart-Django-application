ab18681e252bd5070fb1587d50d2346ebfcb90e9
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-10-07 21:23:03 +0600

    add to cart p-1

--- store/urls.py
+++ store/urls.py
@@ -15,12 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views.home import index
+from .views.home import Index
 from .views.signup import Signup
 from .views.login import Login
 
 urlpatterns = [
-    path('', index, name='homepage'),
+    path('', Index.as_view(), name='homepage'),
     path('signup', Signup.as_view(), name='signup'),
     path('login', Login.as_view(), name='login')
 ]
--- store/views/home.py
+++ store/views/home.py
@@ -1,37 +1,36 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from store.models.product import Product
 from store.models.category import Category
-
+from django.views import View
 
 
 # Create your views here.
 
-# def for product
-def index(request):
-    products = None
-    categories = Category.get_all_categories()
 
-    categoryID = request.GET.get('category')
-    if categoryID:
-        products = Product.get_all_products_by_category_id(categoryID)
-    else:
-        products = Product.get_all_products();
+class Index(View):
 
-    data = {}
-    data['products'] = products
-    data['categories'] = categories
-
-    #printing in terminal to check the session of the customer's email address
-    print("you are: ", request.session.get('customer_email'))
-
-    # return render (request, 'orders/order.html')
-    return render(request, 'index.html', data)
+    def post(self, request):
+        product = request.POST.get('product')
+        return redirect('homepage')
 
 
+    def get(self, request):
+        products = None
+        categories = Category.get_all_categories()
 
+        categoryID = request.GET.get('category')
+        if categoryID:
+            products = Product.get_all_products_by_category_id(categoryID)
+        else:
+            products = Product.get_all_products();
 
+        data = {}
+        data['products'] = products
+        data['categories'] = categories
 
+        # printing in terminal to check the session of the customer's email address
+        print("you are: ", request.session.get('customer_email'))
 
-
-
+        # return render (request, 'orders/order.html')
+        return render(request, 'index.html', data)
