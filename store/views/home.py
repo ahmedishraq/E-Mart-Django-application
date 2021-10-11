6fce61c3bfe0d23566611663269f3a60e5fcaca2
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-10-11 17:12:46 +0600

    cart update p-2

--- store/views/home.py
+++ store/views/home.py
@@ -12,6 +12,18 @@ class Index(View):
 
     def post(self, request):
         product = request.POST.get('product')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product)
+            if quantity:
+                cart[product] = quantity + 1
+            else:
+                cart[product] = 1
+        else:
+            cart = {}
+            cart[product] = 1
+
+        request.session['cart'] = cart
         return redirect('homepage')
 
 
