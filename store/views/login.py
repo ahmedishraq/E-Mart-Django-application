5c4b871c609a4ea93b248321eb3e7265865128e1
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-10-12 00:52:52 +0600

    create logout path for customer

--- store/views/login.py
+++ store/views/login.py
@@ -28,3 +28,7 @@ class Login(View):
         else:
             error_message = "Email or Password invalid !!"
             return render(request, 'login.html', {'error': error_message})
+
+def logout(request):
+    request.session.clear()
+    return redirect('login')
