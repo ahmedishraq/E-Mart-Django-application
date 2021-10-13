9b51502d292cc147778a9c05ed1dfc1bc5e913ae
Ishraq Ahmed <ishraqahmedbd@gmail.com>
2021-10-14 01:00:27 +0600

    efficient way of middleware

--- store/views/orders.py
+++ store/views/orders.py
@@ -6,11 +6,11 @@ from django.views import View
 from store.models.product import Product
 from store.models.orders import Order
 from store.middlewares.auth import auth_middleware
-from django.utils.decorators import method_decorator
+
 
 # class for login (wiil handle GET and POST method)
 class OrderView(View):
-    @method_decorator(auth_middleware)
+
 
     def get(self, request):
         customer = request.session.get('customer')
